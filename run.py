160973f56c4c0273ee54b3f2e318b5a9d015eb8d
Maxim <glusenkom473@gmail.com>
2026-02-17 22:16:29 +0200

    Add auto-migration

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-from app import app
+import os
+from app import app, db
+from flask_migrate import upgrade
+
+# Цей блок автоматично запустить 'flask db upgrade' при старті на сервері
+with app.app_context():
+    # Якщо ми в інтернеті (на Render), запускаємо міграцію
+    if os.environ.get('RENDER'):
+        print("Запуск міграції бази даних...")
+        upgrade()
+        print("Міграція завершена!")
 
 if __name__ == '__main__':
     app.run()
